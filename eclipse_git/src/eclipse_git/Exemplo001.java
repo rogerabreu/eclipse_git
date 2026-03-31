113bd8954c1d4f20f8b36d8a10a98def56f6c1e2
rogerabreu <167481622+rogerabreu@users.noreply.github.com>
2026-03-31 13:58:19 -0300

    Comitando pela 3x.

--- eclipse_git/src/eclipse_git/Exemplo001.java
+++ eclipse_git/src/eclipse_git/Exemplo001.java
@@ -5,6 +5,7 @@ public class Exemplo001 {
 	public static void main(String[] args) {
 		System.out.println("Olá mundo!");
 		System.out.println("Olá mundo 2!");
+		System.out.println("Olá mundo 3!");
 
 	}
 
